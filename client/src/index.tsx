f05be857d581db0a6c4eed51c2cf36745987527b
j253cao <j253cao@uwaterloo.ca>
2023-07-16 00:09:08 -0400

    deploy changes

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
 import "./index.css";
 import App from "./app/App";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+if (process.env.NODE_ENV === "production") disableReactDevTools();
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <App />
